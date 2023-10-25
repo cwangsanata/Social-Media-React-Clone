796d5dff94af536b1d3b0fe2b19864c0d9a12e1c
Chris Wangsanata <chriswangsanata@gmail.com>
2023-10-24 20:16:51 -0700

    add form component

---
+++ client/src/scenes/loginPage/Form.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+function LoginForm() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(`Submitting username ${username} and password ${password}`);
+        
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>
+                Username:
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <br />
+            <label>
+                Password:
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <br />
+            <button type="submit">Log In</button>
+        </form>
+    );
+}
+
+export default LoginForm;
